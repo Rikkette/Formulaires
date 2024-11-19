5e16bad664d3b572acf79a57a0f01abe25aeeb8e
Helene <darte.helene@gmail.com>
2024-11-19 15:14:38 +0000

    formulaire

---
+++ formulaire.js
@@ -0,0 +1,11 @@
+// Le formulaire sera envoyé grâce à la méthode "Post" .
+// La "Société" doit comporter au moins 1 caractère.
+// La "Personne à contacter" doit comporter au moins 1 caractère.
+// Le "Code postal" doit comporter 5 caractères numériques.
+// La "Ville" doit comporter au moins 1 caractère.
+// Le Email doit comporter au moins le caractère "@"
+// alert et return + onSubmit pour html
+
+Text : "Vos Coordonées :";
+
+"Ces zones sont obligatoires pour envoyer le formulaire.";
